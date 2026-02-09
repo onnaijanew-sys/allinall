bcea93eb260cc9bb14ce72dae961bd5324381e5c
onnaijanew-sys <onnaijanew@gmail.com>
2026-02-09 05:33:31 +0100

    Add footer banner ads and scripts

--- multil_saas/includes/footer.php
+++ multil_saas/includes/footer.php
@@ -1,1 +1,11 @@
+<!-- ===== FOOTER BANNER ADS ===== -->
+<div class="ad-container ad-footer">
+    <div style="background:#f1f1f1; padding:10px; border:1px dashed #333; text-align:center;">
+        TEST FOOTER AD
+    </div>
+</div>
 
+<!-- ===== FOOTER SCRIPTS ===== -->
+<script src="../assets/js/main.js"></script>
+</body>
+</html>
