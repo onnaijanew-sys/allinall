2d260d792ca1386a5bb0f7182f2137e00ab511a2
onnaijanew-sys <onnaijanew@gmail.com>
2026-02-09 05:16:33 +0100

    Implement logout functionality with session destruction

--- multil_saas/logout.php
+++ multil_saas/logout.php
@@ -1,1 +1,13 @@
+<?php
+// logout.php
+session_start();
 
+// Destroy all session data
+$_SESSION = [];
+session_unset();
+session_destroy();
+
+// Redirect to login page
+header("Location: index.php");
+exit;
+?>
